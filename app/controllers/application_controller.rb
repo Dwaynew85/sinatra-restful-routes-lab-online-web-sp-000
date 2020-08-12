45772139be5d4259427bb3bdafdf12e7be354a12
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-12 05:52:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
-  # get '/' do
-  #   redirect to '/recipes'
-  # end
+  get '/' do
+    redirect to '/recipes'
+  end
 
   get '/recipes' do
     @recipes = Recipe.all
