5f3387f3821e6f166975fddf53d44b8bdb2e4d49
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-12 06:01:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,12 +29,6 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  post '/recipes' do
-    @recipe = Recipe.create(params)
-
-    redirect to "/recipes/#{@recipe.id}"
-  end
-
   patch '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
@@ -42,6 +36,12 @@ class ApplicationController < Sinatra::Base
     @recipe.cook_time = params[:cook_time]
     @recipe.save
     redirect to "/recipes/#{recipe.id}"
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   delete '/recipes/:id' do
