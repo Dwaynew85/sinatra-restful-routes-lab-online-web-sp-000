eb8a359ef21b335f31b24b4f4b74110d29cbc1d9
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-12 05:46:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
 
 
   post '/recipes' do
-    @recipe = Recipe.new(params)
+    @recipe = Recipe.create(params)
 
     redirect to "/recipes/#{@recipe.id}"
   end
